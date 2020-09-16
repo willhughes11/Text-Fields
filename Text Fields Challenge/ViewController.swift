6dab964bdece1ea90ff2f2d242bd4d4e3db5ac5a
willhughes11 <willhughes2110@gmail.com>
2020-09-15 20:14:23 -0400

    Initial Commit

---
+++ Text Fields Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Text Fields Challenge
+//
+//  Created by William K Hughes on 9/15/20.
+//  Copyright © 2020 William K Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
